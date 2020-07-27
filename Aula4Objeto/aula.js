bef588446c5ea147d559aee1cfb2419b7c013904
Marlon Berkenbrock <35845065+mberkenbrock@users.noreply.github.com>
2020-07-27 14:18:44 -0300

    Add files via upload

---
+++ Aula4Objeto/aula.js
@@ -0,0 +1,21 @@
+let nome = 'Marlon'; //string literal
+let idade = 28; //number literal(inteiro ou decimal)
+let estaAprovado = true; //boolean
+let sobrenome; //undefined
+let peso = 89;
+let profissao = 'Software Developer';
+let cidade  = 'Vancouver';
+
+let pessoa = {
+    nome: 'Marlon',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'Berkenbrock',
+    peso: 89,
+    profissao: 'Software Developer',
+    cidade: 'Vancouver'
+}; //criando objeto
+
+console.log(pessoa);
+
+
---
+++ Aula5Arrays/aula.js
@@ -0,0 +1,5 @@
+let familia = [25,10,1,28,'Marlon',true];
+
+console.log(familia);
+console.log(familia[2]);
+console.log(familia.length);
