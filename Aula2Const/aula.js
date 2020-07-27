e10ede9d7565c1d129119257329b9fbb0790ac8b
Marlon Berkenbrock <35845065+mberkenbrock@users.noreply.github.com>
2020-07-27 14:18:30 -0300

    Add files via upload

---
+++ Aula2Const/aula.js
@@ -0,0 +1,5 @@
+//let valorIngressoAdulto = 20;
+const valorIngressoAdulto = 20;
+//valorIngressoAdulto = 30; const não pode ser alterado
+
+console.log(valorIngressoAdulto);
---
+++ Aula3TIposDados/aula.js
@@ -0,0 +1,8 @@
+let nome = 'Marlon'; //string literal
+let idade = 28; //number literal(inteiro ou decimal)
+let estaAprovado = true; //boolean
+let sobrenome; //undefined
+let corSelecionada = null; //resetar um valor
+
+
+console.log(sobrenome);
