991e120b62439e064198181470e56363087cbf9d
Marlon Berkenbrock <35845065+mberkenbrock@users.noreply.github.com>
2020-07-30 19:39:02 -0300

    Add files via upload

---
+++ Aula28TemplateLiteral/aula.js
@@ -0,0 +1,18 @@
+const mensagem = 'Oi isso é a minha \n teste';
+
+const mensagem2 = `Oi isso é a '"minha"' \n teste`;
+
+console.log(mensagem);
+console.log(mensagem2);
+
+const nome = 'Ricardo';
+const email = 
+`Testando email aqui 124
+Ola, bom dia ${nome}
+Obrigado,
+
+Marlon`;
+
+console.log(email);
+
+
