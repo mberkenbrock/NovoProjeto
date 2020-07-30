6e41156235c3768cafa61a95683c46759563e027
Marlon Berkenbrock <35845065+mberkenbrock@users.noreply.github.com>
2020-07-30 19:38:48 -0300

    Add files via upload

---
+++ Aula27Strings/aula.js
@@ -0,0 +1,17 @@
+//tipo primitido
+const mensagem = '   Testando Primitido teste Bom   ' ;
+
+console.log(mensagem);
+console.log(mensagem.replace('Testando','Minha'));
+console.log(mensagem.includes('Testando'));
+console.log(mensagem.startsWith('te'));
+console.log(mensagem.endsWith('Bom'));
+console.log(mensagem.indexOf('P'));
+console.log(mensagem.trim());
+
+
+
+
+
+//tipo objeto
+const outraMensagem = new String('Bom dia');
